7271f0b3667b84dfc6fbd06a23144675c14a0ed5
Sergio Rupes <sergiop.desarrollador@gmail.com>
2024-08-23 16:38:18 -0500

    UPDATE BUILD

---
+++ assets/axios.93c15a41.js
@@ -0,0 +1,1 @@
+import{b as i,a}from"./index.f25a19a7.js";const r=a.create({});var e=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=r});export{r as api,e as default};
--- assets/axios.c8c25175.js
+++
@@ -1,1 +0,0 @@
-import{b as i,a}from"./index.41a9472d.js";const r=a.create({});var e=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=r});export{r as api,e as default};
